3945fc116669fa5e922367c1ef212adde555fe25
Musa Abubakar <116463104+Mousacr7@users.noreply.github.com>
2026-03-06 01:03:38 +0300

    Update Projects.js

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -11,21 +11,33 @@ export const loadProducts = async () => {
     const snapshot = await getDocs(collection(db, "products"));
     products = snapshot.docs.map((doc) => {
       const data = doc.data();
+    
+      let images = [];
+
+      if (data.category === "tech") {
+        // tech products have multiple images
+        for (let i = 1; i <= 4; i++) {
+          images.push(`/primemart/${data.category}/${data.id}/${data.id}-${i}.webp`);
+        }
+      } else {
+        // other categories only one image
+        images.push(`/primemart/${data.category}/${data.id}.webp`);
+      }
+
       return {
         id: data.id || doc.id,
-        name: data.name ,
-        price: data.price ,
-        category: data.category ,
-        subcategory: data.subcategory ,
-        description: data.description ,
-        color: data.color ,
-        size: data.size ,
-        ram: data.ram ,
-        conect: data.conect ,
-        featured: data.featured ,
-        rating: data.rating ,
-        image: data.image,
-
+        name: data.name,
+        price: data.price,
+        category: data.category,
+        subcategory: data.subcategory,
+        description: data.description,
+        color: data.color,
+        size: data.size,
+        ram: data.ram,
+        conect: data.conect,
+        featured: data.featured,
+        rating: data.rating,
+        images: images
       };
     });
 console.log("Products from Firebase:", products);
